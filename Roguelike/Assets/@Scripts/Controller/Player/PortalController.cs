227e50f618709f92fdbb46891f6373bbe97c68bc
YoungWoongKim <k7y7o756@gmail.com>
2025-05-29 21:43:26 +0900

    0.0.2.5

---
+++ Roguelike/Assets/@Scripts/Controller/Player/PortalController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    public Transform[] StartPoint;
+    public CinemachineCamera[] mapCamera;
+    public ScopeController scope;
+    public bool canPortalInteract = false;
+    public int curSceneNum = 0;
+    public int tempSceneNum = 0;
+    public int nextSceneNum = 0;
+    void Start()
+    {
+        StartCamSetting();
+        transform.position = StartPoint[0].position;
+       
+    }
+    public void StartCamSetting()
+    {
+        for (int i = 1; i < mapCamera.Length; i++)
+        {
+            mapCamera[i].Priority = 1;
+        }
+    }
+    public void UsePotal()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && canPortalInteract == true)
+        {
+            Debug.Log("scope" + $"{nextSceneNum}");
+            scope.ChangeMapCollider(nextSceneNum);
+            transform.position = StartPoint[nextSceneNum].position;
+            scope.transform.position = StartPoint[nextSceneNum].position;
+            StartCoroutine(DelayCamSwitch());
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Portal"))
+        {
+            nextSceneNum = other.GetComponent<Portal>().pNum.portalNum;
+            tempSceneNum = nextSceneNum;
+            canPortalInteract = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Portal"))
+        {
+            nextSceneNum = curSceneNum;
+            canPortalInteract = false;
+        }
+    }
+    IEnumerator DelayCamSwitch()
+    {
+        yield return new WaitForSeconds(0.1f);
+        nextSceneNum = tempSceneNum;
+        
+        yield return new WaitForSeconds(0.9f);
+        
+        mapCamera[curSceneNum].Priority = 0;
+        mapCamera[nextSceneNum].Priority = 20;
+        yield return new WaitForSeconds(0.01f);
+        curSceneNum = nextSceneNum;
+        
+    }
+
+    void Update()
+    {
+        UsePotal();
+    }
+}
