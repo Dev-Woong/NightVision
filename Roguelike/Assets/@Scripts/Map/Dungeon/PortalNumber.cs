ca10d8e69e775cc4d3971828dd65c4018af16f99
Hojin981018 <mhj981018@gmail.com>
2025-06-05 16:06:11 +0900

    0.0.3.0

--- Roguelike/Assets/@Scripts/Map/Dungeon/PortalNumber.cs
+++ Roguelike/Assets/@Scripts/Map/Dungeon/PortalNumber.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PortalNumber", menuName = "Map/Portal")]
 public class PortalNumber : ScriptableObject
 {
+    [Header("맵 생성 시 증가된 번호 추가")]
     public int portalNum;
 
 
---
+++ Roguelike/Assets/@Scripts/UI/Interaction.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    
+    void Start()
+    {   
+
+        foreach (GameObject Gb in gameObjects)
+        {
+            Gb.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            foreach (GameObject Gb in gameObjects)
+            {
+                Gb.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            foreach (GameObject Gb in gameObjects)
+            {
+                Gb.SetActive(false);
+            }
+        }
+    }
+}
