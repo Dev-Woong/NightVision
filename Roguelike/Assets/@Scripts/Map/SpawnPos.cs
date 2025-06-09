a841f4bba2660a3556878f17bf3fd5e545e765bd
krhj-main <k7y7o756@gmail.com>
2025-06-09 10:21:07 +0900

    0.0.3.2

--- Roguelike/Assets/@Scripts/Map/SpawnPos.cs
+++ Roguelike/Assets/@Scripts/Map/SpawnPos.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PortalNumber", menuName = "Map/PortalPos")]
 public class SpawnPoint : MonoBehaviour
 {
     public string spawnId; // ex) "Start", "Checkpoint1", "CaveEntrance"
--- Roguelike/Assets/@Scripts/Utill/DamageBox/AttackData.cs
+++ Roguelike/Assets/@Scripts/Utill/DamageBox/AttackData.cs
@@ -1,40 +1,27 @@
 using UnityEngine;
 
-
-
-
 [CreateAssetMenu(fileName = "SkillData", menuName = "Skill/AttackSkill")]
 public class AttackData : ScriptableObject   
 {
 
-    [Header("타겟")]
     public LayerMask targetMask;
 
-    [Header("무기 타입")]
     public WeaponType weaponType;
 
-    [Header("스킬 불러오는 시점")]
-    [Tooltip("애니메이션 이벤트 생성 프레임 시점")]
-    [TextArea]
     public string animSpot;
 
-    [Header("스킬 히트수")]
     public int hitCount;
 
-    [Header("데미지 (계산 공식은 추후 설정)")]
     public float damageValue;
 
     //[Header("사운드 이펙트")]
     //public AudioClip SFX;
 
-    [Header("타격 이펙트")]
     public GameObject HitEffect;
-    [Header("타격 이펙트 생성 위치")]
+
     public Vector3 effectPos;
 
-    [Header("공격 범위 설정")]
     public Vector3 hitBoxSize;
 
-    [Header("히트박스 생성 위치 설정")]
     public float rangeOffset;
 }
