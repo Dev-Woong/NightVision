74aa3df2ce14739c2fafac33d28d6855b8074a06
Hojin981018 <mhj981018@gmail.com>
2025-06-23 16:11:22 +0900

    0.0.5.2

--- Roguelike/Assets/@Scripts/Map/Object/City.cs
+++ Roguelike/Assets/@Scripts/Map/Object/City.cs
@@ -1,15 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class City : MonoBehaviour
 {
-   
-    public GameObject bd;
+    
+    public Transform bd;
+    public Transform bd1;
+    public Transform bd2;
 
     float h;
     public float speed;
+
     void Start()
-    {
-        speed = 2;        
+    { 
+        speed = 0.05f;      
     }
     void Update()
     {
@@ -18,20 +22,23 @@ public class City : MonoBehaviour
 
     void Move()
     {
-        if (Input.GetButton(Define.Horizontal) /* && (isAttacking == false || weaponType==WeaponType.Gun)*/)
+        if (Input.GetButton(Define.Horizontal))
         {
             h = Input.GetAxisRaw(Define.Horizontal);
-            Vector2 moveDir = new Vector2(h, 0);
-
+            Vector2 moveDir = new Vector2(-h, 0);
+            
             if (Input.GetKey(KeyCode.LeftShift))
             {
-
-                bd.transform.Translate(Time.deltaTime * speed * 2 * moveDir);
+               
+                bd.transform.Translate(Time.deltaTime * (speed * 1.2f) * moveDir);
+                bd1.transform.Translate(Time.deltaTime * (speed * 1.4f) * moveDir);
+                bd2.transform.Translate(Time.deltaTime * (speed * 1.6f) * moveDir);
             }
             else
             {
-
-                bd.transform.Translate(Time.deltaTime * speed * moveDir);
+                bd.transform.Translate(Time.deltaTime * speed  * moveDir);
+                bd1.transform.Translate(Time.deltaTime * (speed / 1.5f) * moveDir);
+                bd2.transform.Translate(Time.deltaTime * (speed / 2f) * moveDir);
             }
         }
     }
