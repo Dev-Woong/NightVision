290cefe956bf205de88d4e40e03ff2c23bcee2ac
Hojin981018 <mhj981018@gmail.com>
2025-06-23 11:12:58 +0900

    0.0.5.1

--- Roguelike/Assets/@Scripts/Map/Object/City.cs
+++ Roguelike/Assets/@Scripts/Map/Object/City.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class City : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+    public GameObject bd;
+
+    float h;
+    public float speed;
     void Start()
     {
-        
+        speed = 2;        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetButton(Define.Horizontal) /* && (isAttacking == false || weaponType==WeaponType.Gun)*/)
+        {
+            h = Input.GetAxisRaw(Define.Horizontal);
+            Vector2 moveDir = new Vector2(h, 0);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+
+                bd.transform.Translate(Time.deltaTime * speed * 2 * moveDir);
+            }
+            else
+            {
+
+                bd.transform.Translate(Time.deltaTime * speed * moveDir);
+            }
+        }
     }
 }
