c061a6f2642ada2186fe5437ada726a069a4f54b
dev-youngwoong <k7y7o756@gmail.com>
2025-08-07 16:59:24 +0900

    0.1.0.8
    Demo Add InvincibleMode

--- Roguelike/Assets/@Scripts/Controller/Player/PlayerStatus.cs
+++ Roguelike/Assets/@Scripts/Controller/Player/PlayerStatus.cs
@@ -16,7 +16,11 @@ public class PlayerStatus: MonoBehaviour
     public int def;
     public int jamStack;
     public float EnergyRecovery;
+    public bool onInvincibleMode = false;
+    public float curInvincibleTriggerTime = 0;
     public Image Energy;
+    public GameObject CheatImageBar;
+    public TMP_Text CheatText;
     public GameObject JamUI;
     public TMP_Text JamText;
     public void EnergyAmount()
@@ -30,6 +34,49 @@ public class PlayerStatus: MonoBehaviour
             curEnergy += Time.deltaTime * EnergyRecovery;
         }
     }
+    public void FilledCheatImage()
+    {
+        
+        CheatImageBar.GetComponent<Image>().fillAmount = curInvincibleTriggerTime / 3;
+    }
+    public void InvincibleModeTrigger()
+    {
+        if (Input.GetKey(KeyCode.P)&&onInvincibleMode ==false)
+        {
+            CheatImageBar.SetActive(true);
+            CheatText.text = "치트모드 활성화중,,,";
+            curInvincibleTriggerTime += Time.deltaTime;
+            if (curInvincibleTriggerTime >3)
+            {
+                onInvincibleMode = true;
+                CheatText.text = "실행 대기중. p를 한번 더 눌러주세요.";
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.P) && onInvincibleMode == false)
+        {
+            curInvincibleTriggerTime = 0;
+            CheatImageBar.SetActive(false);
+        }
+    }
+    IEnumerator CheatTextController()
+    {
+        CheatText.text = "치트 모드 활성화.";
+        yield return new WaitForSeconds(1.5f);
+        CheatImageBar.SetActive(false);
+    }
+    public void OnInvincibleMode()
+    {
+        if (Input.GetKeyDown(KeyCode.P)&&onInvincibleMode == true)
+        {
+            maxHp = float.MaxValue;
+            EnergyRecovery = 100;
+            jamStack = 99999999;
+            status.SetItemStats(this);
+            UpdateJam();
+            StartCoroutine(CheatTextController());
+        }
+    }
+
     public void SetItemStats(ShopItemSlot item)
     {
         atk += item.atk;
@@ -65,6 +112,7 @@ public class PlayerStatus: MonoBehaviour
     {
         curEnergy = maxEnergy;
         status = GetComponent<PublicStatus>();
+        CheatImageBar.SetActive(false);
         UpdateJam();
     }
     void Update()
@@ -73,6 +121,9 @@ public class PlayerStatus: MonoBehaviour
         {
             EnergyCharge();
             EnergyAmount();
+            InvincibleModeTrigger();
+            OnInvincibleMode();
+            FilledCheatImage();
         }
         JamUI.SetActive(!LoadingController.onInputBlocker);
     }
